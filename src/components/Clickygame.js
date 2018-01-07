577155f02a38ac90ed28e1cb34461a308a5183cc
Melanie MacDonald <melmacd94@gmail.com>
2018-01-06 21:57:39 -0500

    fixed error

---
+++ src/components/Clickygame.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import Wrapper from "./Wrapper";
+import "../App.css";
+
+class Clickygame extends Component {
+  render() {
+    return (
+      <Wrapper>
+        hello
+      </Wrapper>
+    );
+  }
+}
+
+export default Clickygame;
