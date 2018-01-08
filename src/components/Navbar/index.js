bbbaf439a93274516e9e080e11996c68d523bbb0
Melanie MacDonald <melmacd94@gmail.com>
2018-01-07 19:01:28 -0500

    dynamic nav

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,17 +1,27 @@
-import React from "react";
+import React, { Component } from 'react';
 import "./Navbar.css";
 
-const Navbar = () =>
-<ul className="nav">
-  <li className="nav-item">
-    Clicky Game
-  </li>
-  <li className="nav-item">
-    Click an image to begin!
-  </li>
-  <li className="nav-item">
-    Score:0 | TopScore:0
-  </li>
-</ul>;
+class Navbar extends Component {
+  state = {
+    message: 'Click any image to begin!',
+    score: '0',
+    topscore: '0'
+  }
+  render(){
+    return(
+      <ul className="nav">
+      <li className="nav-item">
+        Clicky Game
+      </li>
+      <li className="nav-item">
+        {this.state.message}
+      </li>
+      <li className="nav-item">
+        Score: {this.state.score} | Top Score: {this.state.topscore}
+      </li>
+    </ul>
+  );
+}
+}
 
 export default Navbar;
