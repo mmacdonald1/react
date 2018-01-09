380c06849325460f036be05fab92b2fdf474b645
Melanie MacDonald <melmacd94@gmail.com>
2018-01-08 21:47:07 -0500

    not clicking

--- src/components/Clickygame.js
+++ src/components/Clickygame.js
@@ -8,9 +8,37 @@ import "../App.css";
 
 class Clickygame extends Component {
   state = {
-    characters
+    characters,
+    message: 'Click any image to begin!',
+    score: 0,
+    topscore: 0
   };
-//needs to be tested and bring .map in here then call ftn in render
+
+  handleClick = e => {
+  console.log("you clicked")
+  if (!this.state.characters.flipped) {
+      this.state.characters.checkMatch(this.state.characters.id, this.state.characters.flipped);
+      this.setState({flipped:true});
+    }
+    this.randomRender();
+  }
+
+  checkMatch = (id, flipped) => {
+  const cards = this.state.characters;
+    if (cards[id].flipped === false){
+      cards[id].flipped = true;
+      this.setState({score: this.state.score + 1 });
+      this.setState({message:'You guess correctly'});
+      if (this.state.score > this.state.topscore){
+        this.setState({topscore: score})
+      }
+
+   } else if (cards[id].flipped === true) {
+    this.setState({message:'You guessed incorrectly!'});
+    this.setState({score: 0});
+   }
+}
+
   randomChar = (array) => {
     let i = array.length - 1;
      for (; i > 0; i--) {
@@ -24,23 +52,33 @@ class Clickygame extends Component {
 
   randomRender = () => {
     console.log(this.randomChar(this.state.characters))
+    console.log(this)
+
+
     return (
-    this.randomChar(this.state.characters).map(character => (
+      this.randomChar(this.state.characters).map(character => (
 
-      <CharCard
-      key= {character.id}
-      name= {character.name}
-      image= {character.image}
-      />
+        <CharCard
+        className="char"
+        key= {character.id}
+        name= {character.name}
+        image= {character.image}
+        flipped={character.flipped}
+        onClick= {() => {this.handleClick()}}
+        />
 
-    )))}
+      ))
 
-
-
-  render() {
+  )
+  }
+render() {
     return (
       <div>
-        <Navbar />
+        <Navbar
+          message={this.state.message}
+          score={this.state.score}
+          topscore={this.state.topscore}
+         />
 
         <Jumbotron />
 
--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,27 +1,18 @@
-import React, { Component } from 'react';
+import React from 'react';
 import "./Navbar.css";
 
-class Navbar extends Component {
-  state = {
-    message: 'Click any image to begin!',
-    score: '0',
-    topscore: '0'
-  }
-  render(){
-    return(
+const Navbar = props =>
+
       <ul className="nav">
       <li className="nav-item">
         Clicky Game
       </li>
       <li className="nav-item">
-        {this.state.message}
+        {props.message}
       </li>
       <li className="nav-item">
-        Score: {this.state.score} | Top Score: {this.state.topscore}
+        Score: {props.score} | Top Score: {props.topscore}
       </li>
-    </ul>
-  )
-}
-}
+    </ul>;
 
 export default Navbar;
